02e8c873501abcb07aa63b3625c4e37fbf09fa7e
Kim Tae Hoon <eszqsc112@gmail.com>
2024-09-21 20:31:33 +0900

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,21 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+
+  cloud {
+    organization = "thkimzizi"
+    workspaces {
+      name = "skysome"
+    }
   }
 }
 
@@ -16,16 +31,39 @@ variable "region" {
 variable "do_token" {}
 variable "spaces_access_key" {}
 variable "spaces_secret_key" {}
-variable "github_token" {}
+variable "domain_name" {
+  default     = "skysome.one"
+  description = "Domain name to use for the app"
+}
+variable "email" {
+  default     = "eszqsc112@gmail.com"
+  description = "Email address to use for Let's Encrypt"
+}
 
 provider "digitalocean" {
-  token = var.do_token
-
+  token             = var.do_token
   spaces_access_id  = var.spaces_access_key
   spaces_secret_key = var.spaces_secret_key
 }
 
-# Database
+provider "kubernetes" {
+  host  = digitalocean_kubernetes_cluster.skysome_cluster.endpoint
+  token = digitalocean_kubernetes_cluster.skysome_cluster.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    digitalocean_kubernetes_cluster.skysome_cluster.kube_config[0].cluster_ca_certificate
+  )
+}
+
+provider "helm" {
+  kubernetes {
+    host  = digitalocean_kubernetes_cluster.skysome_cluster.endpoint
+    token = digitalocean_kubernetes_cluster.skysome_cluster.kube_config[0].token
+    cluster_ca_certificate = base64decode(
+      digitalocean_kubernetes_cluster.skysome_cluster.kube_config[0].cluster_ca_certificate
+    )
+  }
+}
+
 resource "digitalocean_database_cluster" "skysome_db" {
   name       = "skysome-db"
   engine     = "pg"
@@ -35,62 +73,211 @@ resource "digitalocean_database_cluster" "skysome_db" {
   node_count = 1
 }
 
-# Storage (Spaces)
 resource "digitalocean_spaces_bucket" "skysome_storage" {
   name   = "skysome-storage"
   region = var.region
   acl    = "private"
 }
 
-# Domain
-resource "digitalocean_domain" "skysome_domain" {
-  name = "skysome.one"
-}
+resource "digitalocean_kubernetes_cluster" "skysome_cluster" {
+  name    = "skysome-cluster"
+  region  = var.region
+  version = "1.31.1-do.0"
 
-# App Platform
-resource "digitalocean_app" "skysome_app" {
-  spec {
-    name   = "skysome-app"
-    region = var.region
-
-    service {
-      name               = "skysome-web"
-      instance_size_slug = "basic-xxs"
-      instance_count     = 1
-
-      image {
-        registry_type        = "DOCKER_HUB"
-        registry             = "ghcr.io"
-        repository           = "echoja/skysome"
-        registry_credentials = "echoja:${var.github_token}"
-        tag                  = "latest"
-      }
-
-      http_port = 3000
-
-      env {
-        key   = "DATABASE_URL"
-        value = digitalocean_database_cluster.skysome_db.uri
-      }
-
-      env {
-        key   = "SPACES_ENDPOINT"
-        value = digitalocean_spaces_bucket.skysome_storage.bucket_domain_name
-      }
-    }
-
-    domain {
-      name = digitalocean_domain.skysome_domain.name
-      type = "PRIMARY"
-    }
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-1vcpu-2gb"
+    node_count = 1
   }
 }
 
+resource "kubernetes_namespace" "skysome" {
+  metadata {
+    name = "skysome"
+  }
+  depends_on = [digitalocean_kubernetes_cluster.skysome_cluster]
+}
 
+resource "kubernetes_deployment" "skysome_web" {
+  metadata {
+    name      = "skysome-web"
+    namespace = kubernetes_namespace.skysome.metadata[0].name
+  }
 
-# Output important information
-output "app_url" {
-  value = digitalocean_app.skysome_app.live_url
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "skysome-web"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "skysome-web"
+        }
+      }
+
+      spec {
+        container {
+          image = "node:20"
+          name  = "skysome-web"
+
+          port {
+            container_port = 3000
+          }
+
+          env {
+            name  = "DATABASE_URL"
+            value = digitalocean_database_cluster.skysome_db.uri
+          }
+
+          env {
+            name  = "SPACES_ENDPOINT"
+            value = digitalocean_spaces_bucket.skysome_storage.bucket_domain_name
+          }
+        }
+      }
+    }
+  }
+  depends_on = [digitalocean_kubernetes_cluster.skysome_cluster]
+}
+
+resource "helm_release" "nginx_ingress" {
+  name             = "nginx-ingress"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-nginx"
+  create_namespace = true
+
+  set {
+    name  = "controller.service.type"
+    value = "LoadBalancer"
+  }
+  depends_on = [digitalocean_kubernetes_cluster.skysome_cluster]
+}
+
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+  version          = "v1.11.0"
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+  depends_on = [digitalocean_kubernetes_cluster.skysome_cluster]
+}
+
+resource "digitalocean_domain" "skysome_domain" {
+  name = var.domain_name
+}
+
+resource "digitalocean_record" "www" {
+  domain = digitalocean_domain.skysome_domain.name
+  type   = "A"
+  name   = "@"
+  value  = digitalocean_kubernetes_cluster.skysome_cluster.ipv4_address
+
+  depends_on = [helm_release.nginx_ingress]
+}
+
+resource "kubernetes_ingress_v1" "skysome_ingress" {
+  metadata {
+    name      = "skysome-ingress"
+    namespace = kubernetes_namespace.skysome.metadata[0].name
+    annotations = {
+      "kubernetes.io/ingress.class"              = "nginx"
+      "cert-manager.io/cluster-issuer"           = "letsencrypt-prod"
+      "nginx.ingress.kubernetes.io/ssl-redirect" = "true"
+    }
+  }
+
+  spec {
+    tls {
+      hosts       = [var.domain_name]
+      secret_name = "skysome-tls"
+    }
+
+    rule {
+      host = var.domain_name
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.skysome_web.metadata[0].name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [helm_release.nginx_ingress, kubernetes_manifest.cluster_issuer]
+}
+
+resource "kubernetes_service" "skysome_web" {
+  metadata {
+    name      = "skysome-web"
+    namespace = kubernetes_namespace.skysome.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = kubernetes_deployment.skysome_web.metadata[0].name
+    }
+
+    port {
+      port        = 80
+      target_port = 3000
+    }
+
+    type = "ClusterIP"
+  }
+}
+
+resource "kubernetes_manifest" "cluster_issuer" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "ClusterIssuer"
+    metadata = {
+      name = "letsencrypt-prod"
+    }
+    spec = {
+      acme = {
+        email  = var.email
+        server = "https://acme-v02.api.letsencrypt.org/directory"
+        privateKeySecretRef = {
+          name = "letsencrypt-prod-account-key"
+        }
+        solvers = [
+          {
+            http01 = {
+              ingress = {
+                class = "nginx"
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+
+  depends_on = [helm_release.cert_manager]
+}
+
+output "kubernetes_cluster_name" {
+  value = digitalocean_kubernetes_cluster.skysome_cluster.name
 }
 
 output "database_uri" {
@@ -104,4 +291,8 @@ output "spaces_bucket_name" {
 
 output "domain_name" {
   value = digitalocean_domain.skysome_domain.name
+}
+
+output "load_balancer_ip" {
+  value = digitalocean_kubernetes_cluster.skysome_cluster.ipv4_address
 }
